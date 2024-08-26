6fd1fd0eac3831432fd9cd069b09c7dd8573bd95
eslambob44 <eyasser237@gmail.com>
2024-08-26 12:03:53 +0300

    Game Club Implement Form And Added Games From Another Projects

---
+++ Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Game_club
+{
+    partial class frmGameClub
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGameClub));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbtnLinkedin = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.cbtnTelegram = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.ibtnTypingClub = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.ibtnMemoryGame = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.ibtnTicTacToe = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.ibtnRockPaperScissor = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("OCR A Extended", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(367, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Game Club";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("OCR A Extended", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(9, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Rock Paper Scissor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("OCR A Extended", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(285, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tic-Tac-Toe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("OCR A Extended", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Gold;
+            this.label4.Location = new System.Drawing.Point(515, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Memory Game";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("OCR A Extended", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Gold;
+            this.label5.Location = new System.Drawing.Point(745, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Typing Club";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("OCR A Extended", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Gold;
+            this.label6.Location = new System.Drawing.Point(301, 429);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(317, 30);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Created by Eslam";
+            // 
+            // cbtnLinkedin
+            // 
+            this.cbtnLinkedin.BackgroundImage = global::Game_club.Properties.Resources.LinkedIn;
+            this.cbtnLinkedin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.cbtnLinkedin.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.cbtnLinkedin.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.cbtnLinkedin.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.cbtnLinkedin.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.cbtnLinkedin.FillColor = System.Drawing.Color.Transparent;
+            this.cbtnLinkedin.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.cbtnLinkedin.ForeColor = System.Drawing.Color.White;
+            this.cbtnLinkedin.IndicateFocus = true;
+            this.cbtnLinkedin.Location = new System.Drawing.Point(777, 398);
+            this.cbtnLinkedin.Name = "cbtnLinkedin";
+            this.cbtnLinkedin.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.cbtnLinkedin.Size = new System.Drawing.Size(89, 93);
+            this.cbtnLinkedin.TabIndex = 10;
+            this.cbtnLinkedin.Tag = "https://www.linkedin.com/in/eslam-yasser-143067259/";
+            this.cbtnLinkedin.Click += new System.EventHandler(this.cbtnSocialLink_Click);
+            // 
+            // cbtnTelegram
+            // 
+            this.cbtnTelegram.BackgroundImage = global::Game_club.Properties.Resources.telegram;
+            this.cbtnTelegram.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.cbtnTelegram.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.cbtnTelegram.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.cbtnTelegram.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.cbtnTelegram.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.cbtnTelegram.FillColor = System.Drawing.Color.Transparent;
+            this.cbtnTelegram.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.cbtnTelegram.ForeColor = System.Drawing.Color.White;
+            this.cbtnTelegram.IndicateFocus = true;
+            this.cbtnTelegram.Location = new System.Drawing.Point(87, 398);
+            this.cbtnTelegram.Name = "cbtnTelegram";
+            this.cbtnTelegram.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.cbtnTelegram.Size = new System.Drawing.Size(89, 93);
+            this.cbtnTelegram.TabIndex = 4;
+            this.cbtnTelegram.Tag = "https://t.me/Eslam_BoB";
+            this.cbtnTelegram.Click += new System.EventHandler(this.cbtnSocialLink_Click);
+            // 
+            // ibtnTypingClub
+            // 
+            this.ibtnTypingClub.BackColor = System.Drawing.Color.Transparent;
+            this.ibtnTypingClub.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ibtnTypingClub.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.ibtnTypingClub.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ibtnTypingClub.HoverState.ImageSize = new System.Drawing.Size(128, 128);
+            this.ibtnTypingClub.Image = ((System.Drawing.Image)(resources.GetObject("ibtnTypingClub.Image")));
+            this.ibtnTypingClub.ImageOffset = new System.Drawing.Point(0, 0);
+            this.ibtnTypingClub.ImageRotate = 0F;
+            this.ibtnTypingClub.ImageSize = new System.Drawing.Size(128, 128);
+            this.ibtnTypingClub.IndicateFocus = true;
+            this.ibtnTypingClub.Location = new System.Drawing.Point(735, 239);
+            this.ibtnTypingClub.Name = "ibtnTypingClub";
+            this.ibtnTypingClub.PressedState.ImageSize = new System.Drawing.Size(128, 128);
+            this.ibtnTypingClub.Size = new System.Drawing.Size(173, 111);
+            this.ibtnTypingClub.TabIndex = 3;
+            this.ibtnTypingClub.UseTransparentBackground = true;
+            this.ibtnTypingClub.Click += new System.EventHandler(this.ibtn_Click);
+            // 
+            // ibtnMemoryGame
+            // 
+            this.ibtnMemoryGame.BackColor = System.Drawing.Color.Transparent;
+            this.ibtnMemoryGame.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ibtnMemoryGame.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.ibtnMemoryGame.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ibtnMemoryGame.HoverState.ImageSize = new System.Drawing.Size(128, 128);
+            this.ibtnMemoryGame.Image = ((System.Drawing.Image)(resources.GetObject("ibtnMemoryGame.Image")));
+            this.ibtnMemoryGame.ImageOffset = new System.Drawing.Point(0, 0);
+            this.ibtnMemoryGame.ImageRotate = 0F;
+            this.ibtnMemoryGame.ImageSize = new System.Drawing.Size(128, 128);
+            this.ibtnMemoryGame.IndicateFocus = true;
+            this.ibtnMemoryGame.Location = new System.Drawing.Point(505, 239);
+            this.ibtnMemoryGame.Name = "ibtnMemoryGame";
+            this.ibtnMemoryGame.PressedState.ImageSize = new System.Drawing.Size(128, 128);
+            this.ibtnMemoryGame.Size = new System.Drawing.Size(173, 111);
+            this.ibtnMemoryGame.TabIndex = 2;
+            this.ibtnMemoryGame.UseTransparentBackground = true;
+            this.ibtnMemoryGame.Click += new System.EventHandler(this.ibtn_Click);
+            // 
+            // ibtnTicTacToe
+            // 
+            this.ibtnTicTacToe.BackColor = System.Drawing.Color.Transparent;
+            this.ibtnTicTacToe.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ibtnTicTacToe.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.ibtnTicTacToe.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ibtnTicTacToe.HoverState.ImageSize = new System.Drawing.Size(128, 128);
+            this.ibtnTicTacToe.Image = ((System.Drawing.Image)(resources.GetObject("ibtnTicTacToe.Image")));
+            this.ibtnTicTacToe.ImageOffset = new System.Drawing.Point(0, 0);
+            this.ibtnTicTacToe.ImageRotate = 0F;
+            this.ibtnTicTacToe.ImageSize = new System.Drawing.Size(128, 128);
+            this.ibtnTicTacToe.IndicateFocus = true;
+            this.ibtnTicTacToe.Location = new System.Drawing.Point(275, 239);
+            this.ibtnTicTacToe.Name = "ibtnTicTacToe";
+            this.ibtnTicTacToe.PressedState.ImageSize = new System.Drawing.Size(128, 128);
+            this.ibtnTicTacToe.Size = new System.Drawing.Size(173, 111);
+            this.ibtnTicTacToe.TabIndex = 1;
+            this.ibtnTicTacToe.UseTransparentBackground = true;
+            this.ibtnTicTacToe.Click += new System.EventHandler(this.ibtn_Click);
+            // 
+            // ibtnRockPaperScissor
+            // 
+            this.ibtnRockPaperScissor.BackColor = System.Drawing.Color.Transparent;
+            this.ibtnRockPaperScissor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ibtnRockPaperScissor.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.ibtnRockPaperScissor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ibtnRockPaperScissor.HoverState.ImageSize = new System.Drawing.Size(128, 128);
+            this.ibtnRockPaperScissor.Image = ((System.Drawing.Image)(resources.GetObject("ibtnRockPaperScissor.Image")));
+            this.ibtnRockPaperScissor.ImageOffset = new System.Drawing.Point(0, 0);
+            this.ibtnRockPaperScissor.ImageRotate = 0F;
+            this.ibtnRockPaperScissor.ImageSize = new System.Drawing.Size(128, 128);
+            this.ibtnRockPaperScissor.IndicateFocus = true;
+            this.ibtnRockPaperScissor.Location = new System.Drawing.Point(45, 239);
+            this.ibtnRockPaperScissor.Name = "ibtnRockPaperScissor";
+            this.ibtnRockPaperScissor.PressedState.ImageSize = new System.Drawing.Size(128, 128);
+            this.ibtnRockPaperScissor.Size = new System.Drawing.Size(173, 111);
+            this.ibtnRockPaperScissor.TabIndex = 0;
+            this.ibtnRockPaperScissor.UseTransparentBackground = true;
+            this.ibtnRockPaperScissor.Click += new System.EventHandler(this.ibtn_Click);
+            // 
+            // frmGameClub
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(949, 514);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbtnLinkedin);
+            this.Controls.Add(this.cbtnTelegram);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ibtnTypingClub);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ibtnMemoryGame);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ibtnTicTacToe);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ibtnRockPaperScissor);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmGameClub";
+            this.Text = "Game Club";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2ImageButton ibtnRockPaperScissor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2ImageButton ibtnTicTacToe;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI2.WinForms.Guna2ImageButton ibtnMemoryGame;
+        private System.Windows.Forms.Label label5;
+        private Guna.UI2.WinForms.Guna2ImageButton ibtnTypingClub;
+        private Guna.UI2.WinForms.Guna2CircleButton cbtnTelegram;
+        private Guna.UI2.WinForms.Guna2CircleButton cbtnLinkedin;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using Guna.UI2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tic_Tac_Toe_Game;
+using WindowsFormsApp1;
+
+namespace Game_club
+{
+    public partial class frmGameClub : Form
+    {
+        public frmGameClub()
+        {
+            InitializeComponent();
+        }
+
+        Form frmGame;
+
+        Form _GetForm(Guna2ImageButton ibtn)
+        {
+            if(ibtn==null)
+            {
+                return null;
+            }
+
+            if (ibtn == ibtnRockPaperScissor)
+                return new WindowsFormsApp1.frmMain();
+            else if (ibtn == ibtnTicTacToe)
+                return new frmTicTacToe();
+            else if (ibtn == ibtnMemoryGame)
+                return new Memory_Game.Form1();
+            else if(ibtn == ibtnTypingClub)
+                return new Typing_Club.frmMain();
+
+
+            return null;
+        }
+
+        private void ibtn_Click(object sender, EventArgs e)
+        {
+            frmGame=_GetForm((Guna2ImageButton)sender);
+            if(frmGame!=null)
+                frmGame.ShowDialog();
+        }
+
+        private void cbtnSocialLink_Click(object sender, EventArgs e)
+        {
+            Guna2CircleButton cbtn = sender as Guna2CircleButton;
+            System.Diagnostics.Process.Start(cbtn.Tag.ToString());
+        }
+    }
+}
